4769b1d81601a2cc71a54c6cedcf23fc65482d90
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-18 13:15:04 -0300

    Solucao sem utilizar Generics

---
+++ Generics/PrintService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Generics
+{
+    class PrintService //Solução sem Generics
+    {
+        private int[] Values = new int[10]; 
+        private int Count = 0;
+
+        public void AddValue(int value)
+        {
+            if (Count == 10) //"Programação defensiva"
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+
+            Values[Count] = value;
+            Count++;
+        }
+
+        public int First() 
+        {
+            if (Count == 0) //"Programação defensiva"
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return Values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < Count - 1; i++)
+            {
+                Console.Write(Values[i] + ", ");
+            }
+            if (Count > 0)
+            {
+                Console.Write(Values[Count - 1]);
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * GENERICS:
+            - Generics permitem que classes, interfaces e métodos possam ser parametrizados por tipo. Seus benefícios são:
+                - Reuso
+                - Type safety
+                - Performance
+            - Uso comum: Coleções
+
+            * PROBLEMA EXEMPLO 01:
+            - Deseja-se fazer um programa que leio um conjunto de N números inteiros (N de 1 a 10), e depois imprima esses números de forma organizada conforme exemplo. Em seguida, informar qual foi o primeiro valor informado.
+            EXEMPLO:
+
+            How many value? 3
+            10
+            8
+            23
+            [10, 8, 23]
+            First: 10
+
+            SOLUÇÃO EXEMPLO SEM UTILIZAR GENERICS:
+            */
+
+            PrintService printService = new PrintService(); 
+
+            PrintService printService1 = new PrintService();
+
+            Console.Write("How many value? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
